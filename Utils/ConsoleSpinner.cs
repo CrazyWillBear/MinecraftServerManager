7efdf72cd604dafdaa50a98a4cb02a6c31a7a610
CSPM <crazywillbear@gmail.com>
2021-04-03 23:14:38 -0700

    v0.0.3-prealpha

---
+++ Utils/ConsoleSpinner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace MinecraftServerSoftware.Utils
+{
+    // Code taken from https://stackoverflow.com/questions/1923323/console-animations
+    public class ConsoleSpinner
+    {
+        private const string Sequence = @"/-\|";
+        private int counter = 0;
+        private readonly int delay = 100;
+        private bool active;
+        private readonly Thread thread;
+
+        public ConsoleSpinner()
+        {
+            thread = new Thread(Spin);
+        }
+
+        public void Start()
+        {
+            active = true;
+            if (!thread.IsAlive)
+                thread.Start();
+        }
+
+        public void Stop()
+        {
+            active = false;
+            Draw(' ');
+        }
+
+        private void Spin()
+        {
+            while (active)
+            {
+                Turn();
+                Thread.Sleep(delay);
+            }
+        }
+
+        private void Draw(char c)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\r" + c);
+        }
+
+        private void Turn()
+        {
+            Draw(Sequence[++counter % Sequence.Length]);
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
