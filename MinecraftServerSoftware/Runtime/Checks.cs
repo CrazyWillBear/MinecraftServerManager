26117cbaaa21542c064764878a785ba5919e8091
CaptainBear <crazywillbear@gmail.com>
2021-04-22 16:45:27 -0700

    v0.0.6-alpha rerelease

--- MinecraftServerSoftware/Runtime/Checks.cs
+++ MinecraftServerSoftware/Runtime/Checks.cs
@@ -20,15 +20,10 @@ namespace MinecraftServerSoftware.Runtime
             if (!File.Exists(Program.appdata + @".\uninstall\MinecraftServerSoftware-Uninstaller.exe"))
             {
                 Directory.CreateDirectory(Program.appdata + @".\uninstall");
-                Console.WriteLine("Created directory");
                 GitHubClient client = new GitHubClient(new ProductHeaderValue("MinecraftServerManager"));
-                Console.WriteLine("Created github client");
                 var releases = await client.Repository.Release.GetAll("CrazyWillBear", "MinecraftServerManager");
-                Console.WriteLine("retreived repository releases");
                 var latest = releases[0];
                 WebClient wc = new WebClient();
-                Console.WriteLine("created webclient");
-                Console.WriteLine("https://github.com/CrazyWillBear/MinecraftServerManager/releases/download/" + latest.TagName + "/" + "uninstaller.zip");
                 wc.DownloadFile("https://github.com/CrazyWillBear/MinecraftServerManager/releases/download/" + latest.TagName + "/" + "uninstaller.zip", Program.appdata + @".\uninstall\uninstaller.zip");
                 System.IO.Compression.ZipFile.ExtractToDirectory(Program.appdata + @".\uninstall\uninstaller.zip", Program.appdata + @".\uninstall");
                 File.Delete(Program.appdata + @".\uninstall\MCServerSoftware.zip");
